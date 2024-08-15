e6be8ec4bccd0a70c6e4fa480dc6a0f7e238bac3
Sergey <frankgo14@mail.ru>
2024-08-15 16:39:18 +0500

    Changes in button and sidebar stories

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,7 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Button, { ButtonTheme } from "./Button";
-import { fn } from "@storybook/test";
-import 'app/styles/index.scss';
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import { Theme } from "app/providers/ThemeProvider";
 
@@ -9,11 +7,6 @@ import { Theme } from "app/providers/ThemeProvider";
 const meta: Meta<typeof Button> = {
   title: 'ui/Button',
   component: Button,
-  parameters: {
-    layout: 'centered',
-  },
-  tags: ['autodocs'],
-  args: { onClick: fn() },
 }
 
 export default meta;
