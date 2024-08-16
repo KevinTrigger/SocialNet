120ea41f023ae67b3d173ca60caef5d0228ba438
Sergey <frankgo14@mail.ru>
2024-08-16 14:58:47 +0500

    fix errors in code for github actions settings

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,6 +1,7 @@
+import { StoryFn } from "@storybook/react";
 import { Theme } from "app/providers/ThemeProvider";
 
-export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: StoryFn) => (
   <div className={`app ${theme}`}>
     <StoryComponent />
   </div>
