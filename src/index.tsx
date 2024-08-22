d2cb62fbf021770051512d24f16ae6ab5e8d2c77
Sergey <frankgo14@mail.ru>
2024-08-22 12:12:10 +0500

    change import order in main tsx file (index.tsx)

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "app/App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "app/providers/ThemeProvider";
-import { ErrorBoundary } from "app/providers/ErrorBoundary";
+import { StoreProvider } from "app/providers/StoreProvider";
+import App from "app/App";
 import "app/styles/index.scss";
 import "shared/config/i18n/i18n";
-import { StoreProvider } from "app/providers/StoreProvider";
+import { ErrorBoundary } from "app/providers/ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
