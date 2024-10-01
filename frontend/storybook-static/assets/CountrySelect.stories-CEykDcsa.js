2dca83933209ec6fa0704563ffdcfd7f81ede39e
Sergey <frankgo14@mail.ru>
2024-10-01 15:03:59 +0500

    Test commit for check husky is working

--- frontend/storybook-static/assets/CountrySelect.stories-CEykDcsa.js
+++
@@ -1,6 +0,0 @@
-import{C as p}from"./CountrySelect-CkUT39Yh.js";import{T as n,a as i}from"./ThemeDecorator-DOb0kzc_.js";import"./index-BP8_t0zE.js";import"./jsx-runtime-CexXSJP5.js";import"./classNames-DgDD4s-s.js";import"./Select-DReADGzP.js";const T={title:"entities/CountrySelect",component:p,argTypes:{}},r={args:{}},e={args:{},decorators:[n(i.DARK)]};var o,a,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {}
-}`,...(t=(a=r.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var s,c,m;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {},
-  decorators: [ThemeDecorator(Theme.DARK)]
-}`,...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const S=["Light","Dark"];export{e as Dark,r as Light,S as __namedExportsOrder,T as default};
---
+++ frontend/storybook-static/assets/CountrySelect.stories-Syot8HbK.js
@@ -0,0 +1,6 @@
+import{C as p}from"./CountrySelect-D6tEF-9G.js";import{T as n,a as i}from"./ThemeDecorator-DOb0kzc_.js";import"./index-BP8_t0zE.js";import"./jsx-runtime-CexXSJP5.js";import"./classNames-DgDD4s-s.js";import"./Select-DReADGzP.js";import"./useTranslation-B2QVmqVC.js";const S={title:"entities/CountrySelect",component:p,argTypes:{}},r={args:{}},e={args:{},decorators:[n(i.DARK)]};var o,t,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {}
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var s,m,c;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {},
+  decorators: [ThemeDecorator(Theme.DARK)]
+}`,...(c=(m=e.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const f=["Light","Dark"];export{e as Dark,r as Light,f as __namedExportsOrder,S as default};
